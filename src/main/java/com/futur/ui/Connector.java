c081a5843cb29a9b81c7ec5143cb36f3f2da7ef4
AlexM4Q <Zhelonkin.ru@gmail.com>
2017-03-15 20:20:10 +0400

    quiet formatter

--- src/main/java/com/futur/ui/Connector.java
+++ src/main/java/com/futur/ui/Connector.java
@@ -1,8 +1,11 @@
 package com.futur.ui;
 
 import com.futur.common.helpers.StringHelper;
+import com.google.common.base.Strings;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TextInputControl;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -16,26 +19,32 @@ public final class Connector {
         StringHelper.throwNonInitializeable();
     }
 
-    public static void connectString(@NotNull final TextInputControl textInputControl,
-                                     @NotNull final Consumer<String> consumer) {
-        connect(textInputControl, consumer, Function.identity());
-    }
-
     public static void connectInteger(@NotNull final TextInputControl textInputControl,
                                       @NotNull final Consumer<Integer> consumer) {
-        connect(textInputControl, consumer, Integer::valueOf);
+        connectNumeric(textInputControl, consumer, Integer::valueOf);
     }
 
     public static void connectDouble(@NotNull final TextInputControl textInputControl,
                                      @NotNull final Consumer<Double> consumer) {
-        connect(textInputControl, consumer, Double::valueOf);
+        connectNumeric(textInputControl, consumer, Double::valueOf);
     }
 
-    public static <T> void connect(@NotNull final TextInputControl textInputControl,
-                                   @NotNull final Consumer<T> consumer,
-                                   @NotNull final Function<String, T> function) {
-        textInputControl.textProperty().addListener((observable, oldValue, newValue) ->
-                executeSafe(() -> consumer.accept(function.apply(newValue))));
+    public static <T extends Number> void connectNumeric(@NotNull final TextInputControl textInputControl,
+                                                         @NotNull final Consumer<T> consumer,
+                                                         @NotNull final Function<String, T> function) {
+        textInputControl.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!Strings.isNullOrEmpty(newValue)) {
+                executeSafe(() -> consumer.accept(function.apply(newValue)));
+            }
+        });
+    }
+
+    public static void setValueQuietFormatter(@NotNull final TextInputControl textInputControl,
+                                              @NotNull final String value) {
+        @Nullable final TextFormatter<?> textFormatter = textInputControl.getTextFormatter();
+        textInputControl.setTextFormatter(null);
+        textInputControl.setText(value);
+        textInputControl.setTextFormatter(textFormatter);
     }
 
 }
