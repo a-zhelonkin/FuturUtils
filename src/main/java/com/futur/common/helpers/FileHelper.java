0dd2382ea26eaed82f611458b6c9e701c7cb98fc
AlexM4Q <Zhelonkin.ru@gmail.com>
2017-04-10 19:46:03 +0400

    file helper

---
+++ src/main/java/com/futur/common/helpers/FileHelper.java
@@ -0,0 +1,53 @@
+package com.futur.common.helpers;
+
+import com.google.common.base.Strings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+
+@SuppressWarnings("unused")
+public final class FileHelper {
+
+    private FileHelper() {
+        StringHelper.throwNonInitializeable();
+    }
+
+    public static boolean fileExist(@Nullable final String path) {
+        if (Strings.isNullOrEmpty(path)) {
+            return false;
+        }
+
+        @NotNull final File file = new File(path);
+
+        return file.exists() && file.isFile();
+    }
+
+    @NotNull
+    public static String getFileExtension(@NotNull final File file) {
+        @NotNull final String fileName = file.getName();
+        final int indexOf = fileName.lastIndexOf(".");
+
+        if (indexOf == -1 || indexOf == 0) {
+            return "";
+        } else {
+            return fileName.substring(indexOf);
+        }
+    }
+
+    @NotNull
+    public static String cutNameFromPath(@NotNull final String path) {
+        final int indexOfSlash = path.lastIndexOf("\\");
+        final int indexOfDot = path.indexOf(".");
+
+        return path.substring(indexOfSlash + 1, indexOfDot);
+    }
+
+    @NotNull
+    public static String cutFolderFromPath(@NotNull final String path) {
+        final int indexOfSlash = path.lastIndexOf("\\");
+
+        return path.substring(0, indexOfSlash);
+    }
+
+}
